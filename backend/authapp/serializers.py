7c3d2e2043f2440c70ccb953c6e55e59f4a04be1
JR1053919 <jr1053919@wcupa.edu>
2026-05-10 21:10:00 -0400

    Include nested profile in authapp UserSerializer so /me/ returns image and bio

--- backend/authapp/serializers.py
+++ backend/authapp/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from users.serializers import ProfileSerializer
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role', 'email_verified']
+        fields = [
+            'id', 'username', 'email', 'first_name', 'last_name',
+            'role', 'email_verified', 'profile',
+        ]
         read_only_fields = ('id',)
